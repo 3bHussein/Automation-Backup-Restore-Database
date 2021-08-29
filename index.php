e718b7ddbb4376ac7ad4d38c0eb2fc05b4e251e9
3bHussein <39864308+3bHussein@users.noreply.github.com>
2021-08-29 11:53:25 +0200

    d

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+
+ 
+//  include 'backup.php';
+//  require 'backup.php';
+//   include 'Backup.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="./Backup.php">
+    <input>
+
+
+    <input type="submit" value="Update Database ">
+
+</form>
+    
+
+<form action="/action_page.php">
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname" value="John"><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname" value="Doe"><br><br>
+  <input type="submit" value="Submit">
+</form>
+
+</body>
+</html>
